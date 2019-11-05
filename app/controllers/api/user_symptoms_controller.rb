8a065c1f16e6a54cceb0e212873c988f831026c9
myalisa <myalisamiro@gmail.com>
2019-11-05 11:39:29 -0600

    user_symptom show

--- app/controllers/api/user_symptoms_controller.rb
+++ app/controllers/api/user_symptoms_controller.rb
@@ -1,15 +1,16 @@
 class Api::UserSymptomsController < ApplicationController
   def index
-    @user_symptoms = Usersymptom.all
+    @user_symptoms = UserSymptom.all
     render 'index.json.jb'
     
   end
   
   def create
-    @user_symptom = Usersymptom.new(
-                          user_id: params[:user_id],
+    @user_symptom = UserSymptom.new(
+                          user_id: current_user.id,
                           symptom_id: params[:symptom_id]
                           )
+    @user_symptom.save
     render 'show.json.jb'
   end
 end
